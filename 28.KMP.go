577e80e1dba380657ecfd3f8e8498dc5861a90b4
Dawei Hao <haodawei@xiaiobing.ai>
2022-10-17 09:20:12 +0800

    MOD： Update comment

--- 28.KMP.go
+++ 28.KMP.go
@@ -64,6 +64,7 @@ func strStr(haystack string, needle string) int {
 			j += 1
 		}
 		if j == needleLen-1 {
+			
 			return i - needleLen + 1
 		}
 	}
@@ -76,7 +77,7 @@ func getLPS(pattern string) []int {
 	result := make([]int, 10005, 10010)
 	front := -1
 	// result数组是“最长相等前缀后缀数组”
-	// result[i]表示着，以pattern[i]为结尾的后缀，最长的相同的前缀的长度
+	// result[i]表示着，以pattern[i]为结尾的字符串的后缀，最长的相同的前缀的长度
 	// 比如对于pattern "abbcabb"，result数组为[-1, 0, 0, 0, 1, 2, 3]
 	// 比如我们截取子串: 'abbcab', result[5], 表示了如果以下标5为结尾，那么目前子串前缀后缀最长相等的长度为2，即"ab"
 	// 为啥非忒从减一开始呢？主要是出于使用上的考虑
@@ -89,11 +90,20 @@ func getLPS(pattern string) []int {
 			// 如果当前前缀的最后一个字符和当前后缀的最后一个字符不匹配，那我们就要回退前缀
 			// 比如当前前缀为“aaab”，当前后缀为“aaac”，我们可以把前缀看作模式串，后缀看作待匹配串
 			// ‘b’和‘c’不匹配的时候，我们要回退模式串的下标
-			// 
+			// 回退的时候，我们发现虽然b和c不一样了，但是模式串里的"aaa"和待匹配串里的"aaa"还是一样的，
+			// 回想我们result数组，存放的是以pattern[i]为结尾的字符串后缀，最长的相同的前缀的长度
+			// 那么这个时候，result[2] = 1, 侧面说明了pattern[:1]，是和pattern[-1:]，是重复且完全相等的前后缀
+			// 既然我们能成功匹配到pattern[2]，那说明pattern[:2]肯定是在待匹配串里出现过的
+			// 所以我们不需要把模式串回退到开头，既然都有重复出现的，那就先回退到重复出现的
+			// 所以front = result[2] = 1, 但我们是从-1开始，所以front下一步等于1-1=0
+			// 但是pattern[0+1]，也不等于c，所以再回退一把，front = result[0] = -1，这时候才是真正回退到了模式串开头
+			// 那么我们只要看看，模式串是不是还有前缀
 			front = result[front]
 		}
 
 		if pattern[i] == pattern[front+1] {
+			// 这里记录一个可能导致理解错误的case，比如当模式串为“aaaaaac”的时候，前面这几个“aaaaaa”的result数组值，应该是递增的
+			// 比如[-1,0,1,2,3,4]这种，因为前后缀并没有要求不能有重合部分，只要是连续字串都可以的
 			front += 1
 		}
 		
