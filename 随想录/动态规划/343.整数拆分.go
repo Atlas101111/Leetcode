07d358e4d7c1c15ff46f65d14953144ab72746d6
Dawei Hao <haodawei@xiaiobing.ai>
2023-12-20 17:28:06 +0800

    MOD: refine 343

--- 随想录/动态规划/343.整数拆分.go
+++ 随想录/动态规划/343.整数拆分.go
@@ -49,19 +49,11 @@ func integerBreak(n int) int {
 
 	for i := 2; i <= n; i++ {
 		for j := 1; j <= i/2; j++ {
-			dp[i] = max(dp[i], j*(i-j))
-			dp[i] = max(dp[i], j*dp[i-j])
+			dp[i] = max(dp[i], j*(i-j), j*dp[i-j])
 		}
 	}
 
 	return dp[n]
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
 
 // @lc code=end
